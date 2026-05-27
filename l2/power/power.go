6562ef575ea853ed8cf9485e9317b0bee9fd654e
Alex Flax <alex@alexflax.xyz>
2026-05-27 11:08:13 -0400

    Add support for devices without batteries

--- l2/power/power.go
+++ l2/power/power.go
@@ -14,6 +14,14 @@ func PowerController() {
 		if err != nil {
 			goto DONE	
 		}
+		if len(batteries) < 1 {
+			percent := 100
+			MemoryPower[0] = byte(percent >> 24)
+			MemoryPower[1] = byte(percent >> 16)
+			MemoryPower[2] = byte(percent >> 8)
+			MemoryPower[3] = byte(percent & 0xFF)	
+			goto DONE
+		}
 
 		for _, b := range batteries {
 			percent := uint8((b.Current / b.Full) * 100)
